31101f43a097a3c2b94d37f4237e6c230d622a75
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-04-10 15:49:28 +0100

    implemented pagination. Refactored VideosList code

--- src/Components/VideosList/VideosList.js
+++ src/Components/VideosList/VideosList.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 import { Col, Row } from 'reactstrap';
 import Btn from '../Button/Button';
+import PaginationComponent from '../Pagination';
 import VideoCard from '../VideoCard/VideoCard';
 import './VideosList.css';
 
 function VideosList({ videoList, onClick, setModal, setVideoId, setSource, toggleFavourite, favouriteVideosList }) {
   const [isFavourite, setFavourite] = useState(false)
   const [active, setActive] = useState('All Videos')
-  const videos = videoList;
-  const favouriteVideos = favouriteVideosList;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [videosPerPage] = useState(10);
   const buttons = ['All Videos', 'Favourite Videos']
+  const videosList = isFavourite ? favouriteVideosList : videoList;
 
   const updateStates = button => {
     setActive(button)
@@ -27,15 +29,21 @@ function VideosList({ videoList, onClick, setModal, setVideoId, setSource, toggl
     setFavourite(favourite)
   }
 
+  const indexOfLastVideo = currentPage * videosPerPage;
+  const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
+  const currentVideos = videosList.slice(indexOfFirstVideo, indexOfLastVideo);
+
+  const paginate = pageNumber => setCurrentPage(pageNumber)
+
   return (
     <>
     {buttons.map(button => {
       return (
-        <Btn text={button} size={button === active && 'lg'} className={'videos-list__btn'} onClick={() => updateStates(button)} />
+        <Btn key={button} text={button} size={button === active ? 'lg' : '' } className={'videos-list__btn'} onClick={() => updateStates(button)} />
       )
     })}
       <Row>
-      {!isFavourite && videos.map(video => {
+      {currentVideos.map(video => {
        const { source, image, likes, releaseDate, title, views, id, favourite} = video
         return (
           <Col className="card__column" key={id}>
@@ -55,31 +63,10 @@ function VideosList({ videoList, onClick, setModal, setVideoId, setSource, toggl
               toggleFavourite={toggleFavourite}
             />
           </Col>
-        )
-      })}
-      {isFavourite && favouriteVideos.map(video => {
-        const { source, image, likes, releaseDate, title, views, id, favourite} = video
-        return (
-          <Col className="card__column" key={id}>
-            <VideoCard 
-              source={source}
-              videoId={id}
-              likes={likes}  
-              title={title} 
-              views={views? views : 'Data not found'}
-              publishedDate={releaseDate}
-              image={image}
-              favourite={favourite}
-              onClick={() => onClick(id)}
-              setModal={setModal}
-              setVideoId={setVideoId}
-              setSource={setSource}
-              toggleFavourite={toggleFavourite}
-            />
-          </Col>
-        )
-      })}
+        )})}
+        
     </Row>
+    <PaginationComponent videosPerPage={videosPerPage} totalVideos={videosList.length} paginate={paginate} />
     </>
   );
 }
