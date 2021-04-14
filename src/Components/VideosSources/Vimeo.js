f48a22022b9c543f6bd244e8c9a8d25270588381
Mateusz Rajek <mateusz_rajek@wp.pl>
2021-04-14 12:31:43 +0100

    Data fetching functions split into two separate files and importes to MainView for MainView code cleaning reason

---
+++ src/Components/VideosSources/Vimeo.js
@@ -0,0 +1,17 @@
+import { getVimeoVideoInfo, getVimeoDetailedInfo } from '../../requests'
+
+export const fetchVimeoData = async (inputValue, setVideoList, addVideoToLocalStorage, setInputValue, videoSource, videosList, video) => {
+ 
+  await getVimeoVideoInfo(inputValue).then(async resp => {
+    const { title, upload_date, thumbnail_url, video_id } = resp.data;
+    video = { ...video, source: videoSource.toLowerCase(), title: title, image: thumbnail_url, 
+      releaseDate: upload_date.split(' ')[0], id: video_id };
+    await getVimeoDetailedInfo(video_id).then(async resp => {
+      const likes = resp.data.data[0].metadata.connections.likes.total;
+      video = { ...video, likes: likes };
+      setVideoList([...videosList, video]);
+      addVideoToLocalStorage(video.id, video);
+      setInputValue('');
+    })  
+  }).catch(error => alert(`${error.message} | Please check if your API key, video url or video id are correct`));
+}
